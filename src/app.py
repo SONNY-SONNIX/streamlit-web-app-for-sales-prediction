0de76d80fc490c41c0b72cfd8bd8dee4f47bdab7
Sonny-Sonnix <otchie.sonny@gmail.com>
2023-06-23 14:03:18 +0000

    Updated code

--- src/app.py
+++ src/app.py
@@ -16,41 +16,25 @@ import os
 
 # App 
 
-# Load the trained decision tree model
-# model = DecisionTreeRegressor()  # Replace with your trained model
-decision_tree= DecisionTreeRegressor()
-# Load the data used for prediction
-#data = pd.read_csv('data')  # Replace with your data file or source
-final_dataset = pd.read_csv('final_dataset.csv')
+# Load the trained decision tree model from the pickle file
+model_path = 'decision_tree_model.pkl'
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
 
-# Create the Streamlit web interface
-st.title("Sales Prediction with Decision Tree")
-st.write("Enter the input features and click 'Predict' to get the sales prediction.")
+# Streamlit app layout
+st.title('Sales Prediction')
+st.write('Enter a date to predict sales:')
+date = st.date_input('Date')
 
-# Input form for user
-input_form = st.form(key='sales_prediction_form')
-#feature1 = input_form.number_input("Feature 1")
-#feature2 = input_form.number_input("Feature 2")
-BinaryEncoder = input_form.number_input["encoder_1"]
-OrdinalEncoder= input_form.number_input["encoder_2"]
-# Add more input features as necessary
-
-predict_button = input_form.form_submit_button('Predict')
-
-# Perform prediction when the 'Predict' button is clicked
-if predict_button:
-    # Prepare the input features as a DataFrame for prediction
-    input_data = pd.DataFrame({
-        'BinaryEncoder': [encoder_1],
-        'OrdinalEncoder': [encoder_2]
-        # Add more input features as necessary
-    })
+# Predict sales when the predict button is clicked
+if st.button('Predict'):
+    # Perform any necessary preprocessing on the date input, e.g., feature engineering
     
-    # Make the sales prediction using the trained model
-    prediction = decision_tree.predict(input_data)
+    # Make the prediction using the trained model
+    prediction = model.predict([date])  # Replace [date] with the preprocessed input
+    
+    # Display the predicted sales
+    st.write(f'Predicted sales for {date}: {prediction}')
 
-    # Display the prediction result
-    st.subheader("Sales Prediction")
-    st.write("The predicted sales value is:", prediction)
 
 
