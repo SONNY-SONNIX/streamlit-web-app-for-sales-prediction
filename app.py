d740d45136a1c380b553a0e3654be269bad912ca
Sonny-Sonnix <otchie.sonny@gmail.com>
2023-06-23 07:39:49 +0000

    [Delete] requirement.txt

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 ## Packages imported
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 import numpy as np
 import streamlit as st 
 from PIL import Image
 import pickle 
 import datetime
 import os
+
+
+# Utils 
+
+# Setup & config
+
+
+# App 
