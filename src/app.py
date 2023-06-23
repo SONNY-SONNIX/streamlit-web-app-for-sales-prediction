4cfc909e9c0694c102e967607fc74bbc996f1b6c
Sonny-Sonnix <otchie.sonny@gmail.com>
2023-06-23 14:42:26 +0000

    Updated Image

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
 ## Packages imported
-import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
-import numpy as np
 import streamlit as st 
+import pandas as pd
+#from sklearn.tree import DecisionTreeRegressor
+import numpy as np
 from PIL import Image
 import pickle 
 import datetime
 import os
+from sklearn.tree import DecisionTreeRegressor
+
 
 
 # Utils 
@@ -15,16 +17,26 @@ import os
 
 
 # App 
+# Set page title and layout 
+#Image loader 
+st.image("Z:\GitHub Space\streamlit-web-app-for-sales-prediction\screenshots\hour_glass.jpg")
+
+st.set_page_config(page_title="Favorita Stores Sales Prediction App",layout="wide")
 
 # Load the trained decision tree model from the pickle file
-model_path = 'decision_tree_model.pkl'
+
+model_path = 'saved.pkl'
 with open(model_path, 'rb') as file:
     model = pickle.load(file)
 
+
 # Streamlit app layout
-st.title('Sales Prediction')
+st.title('Favorita Stores Sales Prediction App')
 st.write('Enter a date to predict sales:')
 date = st.date_input('Date')
+
+
+         
 
 # Predict sales when the predict button is clicked
 if st.button('Predict'):
